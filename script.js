8b1413096a630c3225b21ecb26e92fcee1879792
aman kumar <amanjuly2009@gmail.com>
2025-07-31 22:49:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Placeholder for any future JavaScript
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thank you for your message!");
+});
